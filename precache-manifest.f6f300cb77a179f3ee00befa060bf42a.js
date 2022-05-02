69c661debf9e5ec3b743723e778d4878efc39bd3
Yash Samal <samaljyotiraditya@gmail.com>
2022-05-02 21:45:51 +0530

    Updates

---
+++ precache-manifest.f6f300cb77a179f3ee00befa060bf42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1be0ad80e105e4c7789",
+    "url": "/React-Emp-Attr-frontend/static/css/main.12614665.chunk.css"
+  },
+  {
+    "revision": "d1be0ad80e105e4c7789",
+    "url": "/React-Emp-Attr-frontend/static/js/main.37b2b707.chunk.js"
+  },
+  {
+    "revision": "59c4dd964bb764cdd925",
+    "url": "/React-Emp-Attr-frontend/static/js/runtime~main.5e16387d.js"
+  },
+  {
+    "revision": "bd0b6d0eac18ac588762",
+    "url": "/React-Emp-Attr-frontend/static/css/2.0c183857.chunk.css"
+  },
+  {
+    "revision": "bd0b6d0eac18ac588762",
+    "url": "/React-Emp-Attr-frontend/static/js/2.76dda5c8.chunk.js"
+  },
+  {
+    "revision": "86e0e89c8485dafbe1fccfab3930b024",
+    "url": "/React-Emp-Attr-frontend/index.html"
+  }
+];
