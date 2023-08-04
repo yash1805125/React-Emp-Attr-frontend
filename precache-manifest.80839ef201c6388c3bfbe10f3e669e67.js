b568e78d948c4102d1c6c259af9a40126717d471
Yash Samal <samaljyotiraditya@gmail.com>
2023-08-04 13:01:41 +0530

    Deployed React app to GitHub Pages

---
+++ precache-manifest.80839ef201c6388c3bfbe10f3e669e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d6f43cee3679579c62d",
+    "url": "/React-Emp-Attr-frontend/static/css/main.756ec771.chunk.css"
+  },
+  {
+    "revision": "7d6f43cee3679579c62d",
+    "url": "/React-Emp-Attr-frontend/static/js/main.9d2a2085.chunk.js"
+  },
+  {
+    "revision": "59c4dd964bb764cdd925",
+    "url": "/React-Emp-Attr-frontend/static/js/runtime~main.5e16387d.js"
+  },
+  {
+    "revision": "d1f6c39df3e2508e7aca",
+    "url": "/React-Emp-Attr-frontend/static/css/2.04f09cc5.chunk.css"
+  },
+  {
+    "revision": "d1f6c39df3e2508e7aca",
+    "url": "/React-Emp-Attr-frontend/static/js/2.977bf349.chunk.js"
+  },
+  {
+    "revision": "c45febe5c99fc539144fa044150a5909",
+    "url": "/React-Emp-Attr-frontend/index.html"
+  }
+];
